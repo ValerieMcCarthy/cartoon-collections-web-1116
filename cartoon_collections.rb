e4ba2dea206bab969e49e0200afcbbe0d7077035
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-07 18:58:51 -0500

    Planets called and cheese found.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array_names)
+	array_names.each_with_index do |value, index|
+		puts "#{index+1}. #{value}"
+	end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer_calls)
+	planeteer_calls.map! do |x|
+		x.capitalize+"!"
+	end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (calls)
+  long_word = false
+  calls.each do |x|
+  	if x.length > 4
+  		long_word = true
+  	end
+  	end
+  long_word
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_strings.find do |x|
+  	cheese_types.include?(x)
+  end
 end
